4cb941c94f593f9241fc6633cdd5067f2aae599d
Alex Sterrantino <alexsterrantino@gmail.com>
2020-02-07 19:56:01 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -11,7 +11,7 @@ def print_first_directors_movie_titles
   row = 0 
   while row < directors_database.length 
     if col = 0 
-      while col < directors_database.length 
+      while col < directors_database[row].length 
           puts directors_database[row][:movies]
           col += 1 
       end 
