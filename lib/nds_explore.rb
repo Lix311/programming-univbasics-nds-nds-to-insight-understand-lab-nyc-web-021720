951cb2bc5afe134fba236bb177021e5800e98628
Alex Sterrantino <alexsterrantino@gmail.com>
2020-02-07 19:00:35 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -9,20 +9,5 @@ end
 def print_first_directors_movie_titles
   pretty_print_nds(directors_database)
   
-  row = 0 
-  while row < directors_database.length do 
-    #puts "Director #{row} made these movies #{directors_database[row][:movies]}}" # contain 6 hash with inner 4 hash 
-    col = 0 
-    while col < directors_database[row][:movies].length 
-      first_d_movies = directors_database[row][:movies][col][:title]
-        if row == 0 
-        puts first_d_movies
-      end 
-       
-      
-      col += 1 
-    end 
-    
-  row += 1 
-  end      
+  
 end 
